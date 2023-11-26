ae061b5879222d659d5a3aad44c2b3d7e5d3f21f
Sejal <116425408+Sejalmali@users.noreply.github.com>
2023-11-26 15:31:40 +0530

    Delete design_mp5 directory

--- design_mp5/MINIPROJECT.sql
+++
@@ -1,9 +0,0 @@
-create database MINIPROJECT ; 
-use MINIPROJECT ;
-
-CREATE TABLE IF NOT EXISTS users (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        password VARCHAR(255) NOT NULL
-    );
